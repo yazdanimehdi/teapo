fdb23c7809f90082603c7605a05663f9628c79f1
yazdanimehdi <my_jahromi@yahoo.com>
2020-05-27 00:36:35 +0430

    dashboard design completed.

--- src/Views/Dashboard.vue
+++ src/Views/Dashboard.vue
@@ -1,17 +1,333 @@
 <template>
-    <v-app>
-        <v-app-bar>
+    <v-app @click.native="appClick($event)">
+
+        <div class="teapo_title" :style="{'height': `${height*0.08}px`,
+         'width': `${mini? width*0.08 : width*0.230}px`,
+          'max-width': '280px'}">
+
+            <div style="margin: 0" v-if="!mini">
+                <div class="teapo_title_text">TEAPO</div>
+                <div class="teapo_title_desc">Toefl Exam And Practice Online</div>
+            </div>
+            <div v-else>
+                <div class="teapo_title_text_mini">TEAPO</div>
+            </div>
+        </div>
+        <v-app-bar
+                absolute
+                color="#1C0153"
+                dark
+                :height="height*0.08"
+                flat
+                src="../assets/icon_group.png"
+                style="padding: 0; margin: 0;"
+        >
+            <v-spacer></v-spacer>
+            <div style="font-size: 18px; font-family: kalam">Mehdi Yazdani</div>
+            <v-menu
+
+            >
+                <template v-slot:activator="{ on }">
+                    <v-btn icon text v-on="on">
+                        <v-icon>{{icons.mdiChevronDown}}</v-icon>
+                    </v-btn>
+
+                </template>
+
+                <v-list>
+                    <v-list-item link>
+                        <v-list-item-icon :style="{'margin-right': '10px'}">
+                            <v-icon small>
+                                {{ icons.mdiAccountEdit }}
+                            </v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title class="menu_class">Edit Account</v-list-item-title>
+                    </v-list-item>
+
+                    <v-list-item link>
+                        <v-list-item-icon :style="{'margin-right': '10px'}">
+                            <v-icon small>
+                                {{ icons.mdiLogout }}
+                            </v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title class="menu_class">Logout</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
 
         </v-app-bar>
+        <v-navigation-drawer
+                id="navdrawer_element"
+                color="#E6E6E6"
+                :expand-on-hover="false"
+                :mini-variant.sync="mini"
+                :permanent="true"
+                absolute
+                :width="width*0.230"
+                :style="{'padding-top': `${height*0.1}px`, 'max-width': '280px'}"
+                :mini-variant-width="width*0.08"
+        >
+            <div></div>
+            <v-list
+                    nav
+            >
+                <v-list-item link @click="dashboardSelected">
+                    <v-list-item-icon :style="{'margin-right': '10px'}">
+                        <v-icon :color="linkSelected.dashboard ? '#A40000' : '#1C0153'" large>
+                            {{ icons.mdiViewDashboard }}
+                        </v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title class="drawer_links"
+                                           :style="{'color': `${linkSelected.dashboard ? '#A40000' : '#1C0153'}`}"
+                        >Dashboard
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item link @click="TPOSelected">
+                    <v-list-item-icon :style="{'margin-right': '10px'}">
+                        <v-icon :color="linkSelected.TPO ? '#A40000' : '#1C0153'" large>
+                            {{ icons.mdiBookOpenPageVariant }}
+                        </v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title class="drawer_links"
+                                           :style="{'color': `${linkSelected.TPO ? '#A40000' : '#1C0153'}`}"
+                        >TPO
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item link @click="analyticsSelected">
+                    <v-list-item-icon :style="{'margin-right': '10px'}">
+                        <v-icon :color="linkSelected.analytics ? '#A40000' : '#1C0153'" large>
+                            {{ icons.mdiChartTimelineVariant }}
+                        </v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title class="drawer_links"
+                                           :style="{'color': `${linkSelected.analytics ? '#A40000' : '#1C0153'}`}"
+                        >Analytics
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item link @click="wordsSelected">
+                    <v-list-item-icon :style="{'margin-right': '10px'}">
+                        <v-icon :color="linkSelected.words ? '#A40000' : '#1C0153'" large>
+                            {{ icons.mdiBookAlphabet }}
+                        </v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title class="drawer_links"
+                                           :style="{'color': `${linkSelected.words ? '#A40000' : '#1C0153'}`}"
+                        >Words
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item link @click="mockSelected">
+                    <v-list-item-icon :style="{'margin-right': '10px'}">
+                        <v-icon :color="linkSelected.mockTest ? '#A40000' : '#1C0153'" large>
+                            {{ icons.mdiLeadPencil }}
+                        </v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title class="drawer_links"
+                                           :style="{'color': `${linkSelected.mockTest ? '#A40000' : '#1C0153'}`}"
+                        >Mock Test
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item link @click="ordersSelected">
+                    <v-list-item-icon :style="{'margin-right': '10px'}">
+                        <v-icon :color="linkSelected.orders ? '#A40000' : '#1C0153'" large>
+                            {{ icons.mdiCart }}
+                        </v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title class="drawer_links"
+                                           :style="{'color': `${linkSelected.orders ? '#A40000' : '#1C0153'}`}"
+                        >Orders
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+        <DashboardComponent :width="mini? width*0.91 : width*0.76" v-if="linkSelected.dashboard" :style="{'margin-left': mini? `${width*0.08 + 10}px`:`${width*0.23 + 10}px`, 'margin-right': '50px'}"/>
     </v-app>
 </template>
 
 <script>
+    import {
+        mdiViewDashboard,
+        mdiBookOpenPageVariant,
+        mdiChartTimelineVariant,
+        mdiBookAlphabet,
+        mdiLeadPencil,
+        mdiCart,
+        mdiChevronDown,
+        mdiLogout,
+        mdiAccountEdit
+
+    } from '@mdi/js'
+
+    import DashboardComponent from "@/components/DashboardComponent";
+
     export default {
-        name: "Dashboard"
+        name: "Dashboard",
+        components:{
+            DashboardComponent
+        },
+        data() {
+            return {
+                mini: false,
+                icons: {
+                    mdiViewDashboard,
+                    mdiBookOpenPageVariant,
+                    mdiChartTimelineVariant,
+                    mdiBookAlphabet,
+                    mdiLeadPencil,
+                    mdiCart,
+                    mdiChevronDown,
+                    mdiLogout,
+                    mdiAccountEdit
+                },
+                titleHeight: 56,
+                navWidth: 180,
+                width: 0,
+                height: 0,
+                linkSelected: {
+                    dashboard: true,
+                    TPO: false,
+                    words: false,
+                    mockTest: false,
+                    orders: false,
+                    analytics: false,
+                }
+            }
+        },
+        created() {
+            window.addEventListener('resize', this.handleResize);
+            this.handleResize();
+        },
+        destroyed() {
+            window.removeEventListener('resize', this.handleResize);
+        },
+        methods: {
+            dashboardSelected(){
+              this.linkSelected.TPO = false;
+              this.linkSelected.words = false;
+              this.linkSelected.mockTest = false;
+              this.linkSelected.orders = false;
+                this.linkSelected.analytics = false;
+                this.linkSelected.dashboard = true;
+            },
+            TPOSelected(){
+                this.mini = true
+                this.linkSelected.dashboard = false;
+                this.linkSelected.words = false;
+                this.linkSelected.mockTest = false;
+                this.linkSelected.orders = false;
+                this.linkSelected.analytics = false;
+                this.linkSelected.TPO = true;
+            },
+            wordsSelected(){
+                this.linkSelected.dashboard = false;
+                this.linkSelected.TPO = false;
+                this.linkSelected.mockTest = false;
+                this.linkSelected.orders = false;
+                this.linkSelected.analytics = false;
+                this.linkSelected.words = true;
+            },
+            mockSelected(){
+                this.linkSelected.dashboard = false;
+                this.linkSelected.TPO = false;
+                this.linkSelected.words = false;
+                this.linkSelected.orders = false;
+                this.linkSelected.analytics = false;
+                this.linkSelected.mockTest = true;
+            },
+            ordersSelected(){
+                this.linkSelected.dashboard = false;
+                this.linkSelected.TPO = false;
+                this.linkSelected.words = false;
+                this.linkSelected.mockTest = false;
+                this.linkSelected.analytics = false;
+                this.linkSelected.orders = true;
+            },
+            analyticsSelected(){
+                this.linkSelected.dashboard = false;
+                this.linkSelected.TPO = false;
+                this.linkSelected.words = false;
+                this.linkSelected.mockTest = false;
+                this.linkSelected.orders = false;
+                this.linkSelected.analytics = true
+            },
+            handleResize() {
+                this.width = window.innerWidth;
+                this.height = window.innerHeight;
+            },
+            appClick(event){
+                let myElementToCheckIfClicksAreInsideOf = document.querySelector('#navdrawer_element');
+                if (!myElementToCheckIfClicksAreInsideOf.contains(event.target)) {
+                    this.mini = true;
+                }
+            }
+        }
     }
 </script>
 
 <style scoped>
+    .teapo_title {
+        background-color: #a9a1ba;
+        margin: 0;
+        padding: 0;
+        position: relative;
+        z-index: 1000;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 
+    .teapo_title_text {
+        color: #1C0153;
+        font-weight: bold;
+        text-align: center;
+        font-size: 22px;
+        font-family: kalam;
+    }
+    .teapo_title_text_mini {
+        color: #1C0153;
+        font-weight: bold;
+        text-align: center;
+        font-size: 20px;
+        margin-top: 5px;
+        font-family: kalam;
+    }
+
+    .teapo_title_desc {
+        color: #1C0153;
+        font-size: 12px;
+        text-align: center;
+        font-family: kalam;
+
+    }
+
+    .drawer_links {
+        font-family: kalam;
+        font-size: 22px
+    }
+    .menu_class{
+        font-family: kalam;
+        font-size: 14px;
+    }
 </style>
---
+++ src/components/DashboardComponent.vue
@@ -0,0 +1,227 @@
+<template>
+    <div :style="{'width': `${width}px`, 'font-family':'kalam'}">
+        <v-container fluid>
+            <v-row>
+                <v-col cols="12" sm="8" md="8" lg="8" xl="8">
+                    <v-card class="mx-auto"
+                            style="background-image: linear-gradient(#DF55B1, #CEA0BF); height: 300px; border-radius: 15px"
+                    >
+                        <v-img src="../assets/new_releases.png" class="white--text" contain
+                               height="300px">
+                            <v-card-subtitle>
+                                <div>
+                                    <v-icon>{{icons.mdiAlertDecagram}}</v-icon>
+                                    <span style="margin-left: 8px; font-size: 18px; font-weight: bold">What's New</span>
+                                </div>
+
+                            </v-card-subtitle>
+
+                        </v-img>
+                    </v-card>
+
+                </v-col>
+                <v-col cols="12" sm="4" md="4" lg="4" xl="4">
+                    <v-card style="background-image: linear-gradient(#5362C5, #676D9A); height: 300px; border-radius: 15px">
+                        <v-img src="../assets/inbox.png" class="white--text" contain
+                               height="300px">
+                            <v-card-subtitle>
+                                <div>
+                                    <v-icon>{{icons.mdiInboxArrowDown}}</v-icon>
+                                    <span style="margin-left: 8px; font-size: 18px; font-weight: bold">Inbox</span>
+                                </div>
+
+                            </v-card-subtitle>
+                        </v-img>
+                    </v-card>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col cols="12" sm="3" md="3" lg="3" xl="3">
+                    <v-card style="background-image: linear-gradient(#2EC9B9, #8FB9B3); height: 240px; border-radius: 15px">
+                        <v-img src="../assets/sand.png" class="black--text" contain
+                               height="240px">
+                            <v-card-subtitle>
+                                <div>
+                                    <v-icon>{{icons.mdiTimerSandEmpty}}</v-icon>
+                                    <span style="margin-left: 8px; font-size: 18px; font-weight: bold">Time</span>
+                                </div>
+                            </v-card-subtitle>
+                            <v-card-text>
+                                <div style="font-weight: bolder; text-align: center; font-size: 50px; margin-bottom: 20px; margin-top: 20px">
+                                    38
+                                </div>
+                                <div style="font-weight: bolder; text-align: center; font-size: 25px; margin-bottom: 20px;">
+                                    Days left
+                                </div>
+                                <div style="text-align: center; font-size: 25px">
+                                    Until your toefl
+                                </div>
+                            </v-card-text>
+                        </v-img>
+                    </v-card>
+                </v-col>
+                <v-col cols="12" sm="6" md="6" lg="6" xl="6">
+                    <v-card style="background-image: linear-gradient(#E29578, #CCB9B2); height: 240px; border-radius: 15px">
+                        <v-img src="../assets/fast_forward.png" class="black--text" contain
+                               height="240px">
+                            <v-card-subtitle>
+                                <div>
+                                    <v-icon>{{icons.mdiFastForward}}</v-icon>
+                                    <span style="margin-left: 8px; font-size: 18px; font-weight: bold">Progress</span>
+                                </div>
+
+                            </v-card-subtitle>
+                            <v-card-text>
+                                <v-container style="margin: 0">
+                                    <v-row>
+                                        <v-col cols="6" sm="6" md="6" lg="6" xl="6" style="margin: 0; padding: 0">
+                                            <div style="text-align: left; font-size: 20px;">
+                                                Reading: 42/63
+                                            </div>
+                                            <div style="text-align: left; font-size: 20px; margin-top: 20px">
+                                                Listening: 42/63
+                                            </div>
+                                            <div style="text-align: left; font-size: 20px; margin-top: 20px">
+                                                Speaking: 42/63
+                                            </div>
+                                            <div style="text-align: left; font-size: 20px; margin-top: 20px">
+                                                Writing: 42/63
+                                            </div>
+                                        </v-col>
+                                        <v-col cols="6" sm="6" md="6" lg="6" xl="6">
+                                            <div style="text-align: center; font-size: 20px; font-weight: bold; margin-top: 10px">
+                                                Total Progress
+                                            </div>
+                                            <div style="text-align: center; font-size: 30px; font-weight: bold; margin-top: 10px">
+                                                58%
+                                            </div>
+                                        </v-col>
+                                    </v-row>
+                                </v-container>
+
+
+                            </v-card-text>
+                        </v-img>
+                    </v-card>
+                </v-col>
+                <v-col cols="12" sm="3" md="3" lg="3" xl="3">
+                    <v-card style="background-image: linear-gradient(#FF5D5D, #D29E9E);height: 240px;  border-radius: 15px">
+                        <v-img src="../assets/history.png" class="white--text" contain
+                               height="240px">
+                            <v-card-subtitle>
+                                <div>
+                                    <v-icon>{{icons.mdiHistory}}</v-icon>
+                                    <span style="margin-left: 8px; font-size: 18px; font-weight: bold">History</span>
+                                </div>
+
+                            </v-card-subtitle>
+                        </v-img>
+
+                    </v-card>
+                </v-col>
+            </v-row>
+
+            <v-row>
+                <v-col cols="12" sm="7" md="7" lg="7" xl="7">
+                    <v-card style="background-image: linear-gradient(#C9D8C3, #10DC2B); height: 200px; border-radius: 15px">
+                        <v-img src="../assets/course.png" class="white--text" contain
+                               height="200px">
+                            <v-card-subtitle>
+                                <div>
+                                    <v-icon>{{icons.mdiLeadPencil}}</v-icon>
+                                    <span style="margin-left: 8px; font-size: 18px; font-weight: bold">Practice Now</span>
+                                </div>
+
+                            </v-card-subtitle>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn icon>
+                                    <v-icon x-large>{{icons.mdiArrowRightBold}}</v-icon>
+                                </v-btn>
+                            </v-card-actions>
+                        </v-img>
+
+                    </v-card>
+                </v-col>
+                <v-col cols="12" sm="5" md="5" lg="5" xl="5">
+                    <v-card style="background-image: linear-gradient(#A0B271, #B6F51F); height: 200px; border-radius: 15px">
+                        <v-img src="../assets/study.png" class="black--text" contain
+                               height="200px">
+                            <v-card-subtitle>
+                                <div>
+                                    <v-icon>{{icons.mdiBookAlphabet}}</v-icon>
+                                    <span style="margin-left: 8px; font-size: 18px; font-weight: bold">Study Now</span>
+                                </div>
+
+                            </v-card-subtitle>
+                            <v-card-text>
+                                <v-container>
+                                    <v-row>
+                                        <v-col cols="4" sm="4" md="4" lg="4" xl="4" >
+                                            <div style="font-weight: bolder; font-size: 50px; text-align: center">26</div>
+                                        </v-col>
+                                        <v-col cols="4" sm="4" md="4" lg="4" xl="4" >
+                                            <div style="font-weight: bold; font-size: 20px; text-align: center">Words</div>
+                                            <div style="font-size: 20px; text-align: center">for review</div>
+                                        </v-col>
+                                        <v-col cols="2" sm="2" md="2" lg="2" xl="2">
+                                            <v-btn icon>
+                                                <v-icon x-large>{{icons.mdiArrowRightBold}}</v-icon>
+                                            </v-btn>
+                                        </v-col>
+                                    </v-row>
+                                </v-container>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+
+                            </v-card-actions>
+                        </v-img>
+
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+    import {
+        mdiAlertDecagram,
+        mdiInboxArrowDown,
+        mdiTimerSandEmpty,
+        mdiFastForward,
+        mdiHistory,
+        mdiLeadPencil,
+        mdiBookAlphabet,
+        mdiArrowRightBold
+    } from '@mdi/js';
+
+    export default {
+        name: "DashboardComponent",
+        props: {
+            width: {
+                required: true,
+                type: Number
+            }
+        },
+        data() {
+            return {
+                icons: {
+                    mdiAlertDecagram,
+                    mdiInboxArrowDown,
+                    mdiTimerSandEmpty,
+                    mdiFastForward,
+                    mdiHistory,
+                    mdiLeadPencil,
+                    mdiBookAlphabet,
+                    mdiArrowRightBold
+
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
